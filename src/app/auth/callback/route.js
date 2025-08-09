5ef8627e7ed82022cd6608239fcbb932aa15b41b
silveron19 <silverterkristian@gmail.com>
2025-08-09 12:19:45 +0800

    feat: fix callback bug

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,19 +1,23 @@
 import { createSupabaseServerClient } from '@/utils/supabase/supabaseServer';
 import { NextResponse } from 'next/server';
 
-export async function GET(request) {
+export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/cari-video';
-
+  let next = searchParams.get('next') ?? '/';
+  if (!next.startsWith('/')) {
+    // if "next" is not a relative URL, use the default
+    next = '/';
+  }
   if (code) {
-    const supabase = await createSupabaseServerClient();
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host');
+      const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development';
       if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
@@ -22,6 +26,6 @@ export async function GET(request) {
       }
     }
   }
-
+  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
--- src/app/login/action.js
+++ src/app/login/action.js
@@ -6,10 +6,22 @@ import { redirect } from 'next/navigation';
 export async function signInWithGoogle() {
   const supabase = createSupabaseBrowserClient();
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/';
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`;
+    return url;
+  };
+
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+      redirectTo: getURL(),
       queryParams: {
         access_type: 'offline',
         prompt: 'consent',
